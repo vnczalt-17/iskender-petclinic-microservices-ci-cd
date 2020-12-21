5ce38fc5014c3e48c5cf0d426f7c8e5690da36a9
vnczalt-17 <vnczalt.17@gmail.com>
2020-12-21 21:22:18 +0000

    updated jenkins via webhook

--- petclinic-microservices/jenkins/jenkins-petclinic-ci-job.sh
+++ petclinic-microservices/jenkins/jenkins-petclinic-ci-job.sh
@@ -1,2 +1,2 @@
 echo 'Running Unit Tests on Petclinic Application'
-docker run --rm -v $HOME/.m2:/root/.m2 -v `pwd`:/app -w /app maven:3.6-openjdk-11 mvn clean test
+docker run --rm -v $HOME/.m2:/root/.m2 -v $WORKSPACE/petclinic-microservices:/app -w /app maven:3.6-openjdk-11 mvn clean test
